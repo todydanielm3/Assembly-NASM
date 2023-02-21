6a263ff87b29d68d62008dca7b422c086b21ac00
todydanielm3 <daniel8moraess@gmail.com>
2023-02-21 21:29:38 +0000

    olm

--- olaMundo.asm
+++ olaMundo.asm
@@ -1,11 +1,16 @@
-section   .text
-org 0x100
-  mov ah, 0x9
-  mov dx, ola
-  int 0x21
+section   .data
+	msg db 'ola mundo' ,0 
 
-  mov ax, 0x4c00
-  int 0x21
+section .text
+	global _start
 
-section .data
-ola:  db 'ola mundoo!', 11,9, '$'
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx,msg
+	mov edx,12
+	int 0x80
+
+	mov eax,1
+	xor ebx, ebx
+	int 0x80
